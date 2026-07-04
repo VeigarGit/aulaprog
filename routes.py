afe15ce764036c77cb2fa0f6c86457bb182e75ea
theo <theomello32@gmail.com>
2026-07-03 23:30:03 -0300

    Finalizando o Routes

--- routes.py
+++ routes.py
@@ -1,7 +1,15 @@
 from http.server import BaseHTTPRequestHandler
-
+from database import listar_tarefas
 class GerenciadorHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        tarefas = listar_tarefas()
+        linhas_html = ""
+        for tarefa in tarefas:
+            linhas_html += f"<li>{tarefa['titulo']} - {tarefa['status']}</li>"
+        html = f"<h1> minhas tarefas </h1><ul>{linhas_html}</ul>"
+        
+
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
