d3eaf659357417df9118d2561eb0510bca5a3341
theo <theomello32@gmail.com>
2026-07-03 23:22:09 -0300

    Progressos

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from http.server import HTTPServer
+from routes import GerenciadorHandler
 from database import criar_tabelas
 
 
 
 def main():
     criar_tabelas()
+    servidor = HTTPServer(("localhost", 8000), GerenciadorHandler)
+    servidor.serve_forever()
 
 
 if __name__ == "__main__":
